241530bdf9c0239d07f6f0756dd2ce76efdabf4c
Jack Tierney <jctierney251@gmail.com>
2016-05-11 11:53:46 +0100

    form bs

--- public/script.js
+++ public/script.js
@@ -1,13 +1,17 @@
-const addUser = () => {
-  const xhttp = new XMLHttpRequest();
+var addUser = () => {
+  var xhttp = new XMLHttpRequest();
   console.log("submitted");
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       console.log(xhttp.responseText)
     }
   };
-  xhttp.open("GET", "/addUser" + document.getElementsById('number') + "/" + document.getElementsById("credit"));
+  var url = "/addUser/" + document.getElementsById('number').input + "/" + document.getElementsById("credit").input;
+  console.log(url);
+  xhttp.open("GET", "/addUser/" + document.getElementsById('number').input + "/" + document.getElementsById("credit").input);
   xhttp.send();
 }
 
-document.getElementById("submit").addEventListener('submit', addUser)
+document.getElementById('but').addEventListener('click', thing)
+
+const thing = () => console.log('hi')
